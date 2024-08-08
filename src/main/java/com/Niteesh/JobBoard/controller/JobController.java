fa8e4bf0e2c02d42dccec4dcd1bd3c8aa0a9aba0
niteesh-chowdary <niteesh.chowdary25@gmail.com>
2024-08-07 23:05:26 -0400

    Implemented CRUD operations; Setup & Configured JPA, and H2-DB

--- src/main/java/com/Niteesh/JobBoard/controller/JobController.java
+++ src/main/java/com/Niteesh/JobBoard/controller/JobController.java
@@ -2,16 +2,14 @@ package com.Niteesh.JobBoard.controller;
 
 import com.Niteesh.JobBoard.model.Job;
 import com.Niteesh.JobBoard.service.JobService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/jobs")
 public class JobController {
 
     private JobService jobService;
@@ -20,14 +18,47 @@ public class JobController {
         this.jobService = jobService;
     }
 
-    @GetMapping("/jobs")
-    public List<Job> findAll(){
-        return jobService.findAll();
+    @GetMapping
+    public ResponseEntity<List<Job>> findAll(){
+        List<Job> jobs = jobService.findAll();
+        if(jobs != null)
+            return new ResponseEntity<>(jobs,HttpStatus.OK);
+        else return new ResponseEntity<>(jobs,HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
-        jobService.createJob(job);
-        return "Job added successfully";
+    @PostMapping
+    public ResponseEntity<String> createJob(@RequestBody Job job){
+        String jobPosted = jobService.createJob(job);
+        if(jobPosted.equals("JOB CREATED"))
+            return new ResponseEntity<>(jobPosted,HttpStatus.CREATED);
+        else return new ResponseEntity<>(jobPosted,HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @GetMapping("/{jobId}")
+    public ResponseEntity<Job> findJob(@PathVariable Long jobId){
+        Job job = jobService.findJob(jobId);
+        if(job != null)
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{jobId}")
+    public ResponseEntity<String> deleteJobById(@PathVariable Long jobId){
+        Job job = jobService.findJob(jobId);
+        if(job == null){
+            return new ResponseEntity<>("NO ITEM",HttpStatus.NOT_FOUND);
+        }
+        else {
+            jobService.deleteJobById(jobId);
+            return new ResponseEntity<>("SUCCESSFULLY DELETED", HttpStatus.OK);
+        }
+    }
+
+    @PutMapping("/{jobId}")
+    public ResponseEntity<?> updateJobById(@PathVariable Long jobId, @RequestBody Job job){
+        String jobUpdateStatus = jobService.updateJobById(jobId,job);
+        if(jobUpdateStatus.equals("JOB UPDATED"))
+            return new ResponseEntity<>(jobUpdateStatus,HttpStatus.OK);
+        else return new ResponseEntity<>(jobUpdateStatus,HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/Niteesh/JobBoard/service/JobService.java
+++ src/main/java/com/Niteesh/JobBoard/service/JobService.java
@@ -1,19 +1,43 @@
 package com.Niteesh.JobBoard.service;
 
 import com.Niteesh.JobBoard.model.Job;
+import com.Niteesh.JobBoard.repository.JobRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class JobService {
-    List<Job> jobs = new ArrayList<>();
-    public List<Job> findAll(){
-        return jobs;
+    JobRepository repository;
+
+    public JobService(JobRepository repository){
+        this.repository = repository;
     }
 
-    public void createJob(Job job){
-        jobs.add(job);
+    public List<Job> findAll(){
+        return repository.findAll();
+    }
+
+    public String createJob(Job job){
+        Job jobCreated = repository.save(job);
+        if(jobCreated != null)
+            return "JOB CREATED";
+        else return "JOB CREATION FAILED";
+    }
+
+    public Job findJob(Long jobId) {
+        return repository.findById(jobId).orElse(null);
+    }
+
+    public void deleteJobById(Long jobId) {
+        repository.deleteById(jobId);
+    }
+
+    public String updateJobById(Long jobId, Job job) {
+        Job jobCreated = repository.findById(jobId).orElse(null);
+        if(jobCreated != null) {
+            repository.save(job);
+            return "JOB UPDATED";
+        }
+        else return "JOB UPDATE FAILED";
     }
 }
